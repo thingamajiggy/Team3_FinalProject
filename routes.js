9ad2fe2bc1ee969f37bf50f203456595a5984e5a
Hye Rin Eom <hyerineom@awylie.local>
2022-10-11 10:35:43 +0100

    add patch and delete

--- routes.js
+++ routes.js
@@ -6,7 +6,9 @@ const Supplier = require("./model/supplier");
 const router = express.Router();
 
 //POSTS
+
 router.post("/customer/", (req, res) => {
+
   const { name, phoneNumber, orderDate, furnitureOrder } = req.body;
 
   const customer = new Customer({
@@ -18,7 +20,6 @@ router.post("/customer/", (req, res) => {
 
   try {
     const dataToSave = customer.save();
-
     res.status(200).json(dataToSave);
   } catch (err) {
     res.status(400).json({ message: err.message });
@@ -119,7 +120,37 @@ router.get("/components/:id", async (req, res) => {
     res.status(500).json({ message: error.message });
   }
 });
+
 //PATCH
 
+router.patch('/components/:id', async (req, res) => {
+  try {
+      const id = req.params.id;
+      const updatedData = req.body;
+      const options = { new: true };
+
+      const result = await Model.findByIdAndUpdate(
+          id, updatedData, options
+      )
+
+      res.send(result)
+  }
+  catch (error) {
+      res.status(400).json({ message: error.message })
+  }
+})
+
 //DELETE
+
+router.delete('/components/:id', async (req, res) => {
+  try {
+      const id = req.params.id;
+      const data = await Model.findByIdAndDelete(id)
+      res.send(`Document with ${data.id} has been deleted..`)
+  }
+  catch (error) {
+      res.status(400).json({ message: error.message })
+  }
+})
+
 module.exports = router;
