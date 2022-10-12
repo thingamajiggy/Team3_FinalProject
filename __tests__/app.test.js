b267bf2dc935a65dcd926727bbc81448065bd5c6
Hye Rin Eom <hyerineom@awylie.local>
2022-10-12 11:59:40 +0100

    Change the test suite

---
+++ __tests__/app.test.js
@@ -0,0 +1,100 @@
+const mongoose = require('mongoose');
+const request = require('supertest');
+const app = require('../app');
+const Components = require('../components');
+require('dotenv').config();
+/*connecting to the database before each test. */
+
+beforeEach(async () => {
+  await mongoose.connect(process.env.DATABASE_URL);
+});
+/*closing the connection after each test. */
+afterEach(async () => {
+  await mongoose.connection.close();
+});
+
+//CREATE-POST
+describe('POST /components', () => {
+  it('should create a new component', async () => {
+    const id = 100;
+    const component = {
+      id,
+      component: 'swallow-tail drawer back',
+      stockLevel: 24,
+      triggerPoint: 12,
+    }
+    jest.spyOn(Components.prototype, 'save')
+      .mockImplementationOnce(() => component)
+    const res = await request(app).post('/api/components').send();
+    expect(res.statusCode).toBe(201);
+    expect(res.body.component).toBe(component.component);
+    expect(res.body).hasOwnProperty(id);
+  });
+});
+
+//READ-GET
+describe('GET /components', () => {
+  it('should return all components', async () => {
+    const res = await request(app).get('/api/components');
+    expect(res.statusCode).toBe(200);
+    expect(res.body).toBeInstanceOf(Array);
+    expect(res.body[1]).toBeInstanceOf(Object);
+    expect(res.body.length).toBeGreaterThan(0);
+  });
+});
+
+describe('GET /components/:id', () => {
+  it('should return a component with the matching id number', async () => {
+
+    const id = 1
+    const component = {
+      id,
+      component: 'to be decided',
+      stockLevel: 42,
+      triggerPoint: 21,
+    }
+    const findByIdSpy = jest.spyOn(Components, 'findById');
+    findByIdSpy.mockImplementation(() => component)
+
+    const res = await request(app).get(`/api/components/${id}`);
+    expect(res.statusCode).toBe(200);
+    expect(res.body).toEqual(component);
+    findByIdSpy.mockRestore();
+  });
+});
+
+//UPDATE-PATCH
+describe('PATCH /components/:id', () => {
+  it('should update a product', async () => {
+    const id = 2
+    const component = {
+      component: 'dove-tailed joint drawer back',
+      stockLevel: 24,
+      triggerPoint: 12,
+    }
+
+    const findByIdAndUpdateSpy = jest.spyOn(Components, 'findByIdAndUpdate');
+    findByIdAndUpdateSpy.mockImplementation(() => ({ ...component, id }))
+
+    const res = await request(app)
+      .patch(`/api/components/${id}`)
+      .send(component);
+    expect(findByIdAndUpdateSpy).toHaveBeenCalled()
+    expect(res.statusCode).toBe(201);
+    expect(res.body.component).toBe('dove-tailed joint drawer back');
+    expect(res.body.id).toBe(2);
+    findByIdAndUpdateSpy.mockRestore();
+  });
+});
+
+//DELETE-DELETE
+describe('DELETE /api/components/:id', () => {
+  it('should delete a product', async () => {
+    const id = 2
+    const findByIdAndDeleteSpy = jest.spyOn(Components, ‘findByIdAndDelete’);
+    findByIdAndDeleteSpy.mockImplementation(() => ({ id }))
+    const res = await request(app).delete(`/api/components/${id}`);
+    expect(res.statusCode).toBe(200);
+    findByIdAndDeleteSpy.mockRestore();
+  });
+});
--- routes.js
+++ routes.js
@@ -2,50 +2,7 @@ const express = require("express");
 const Customer = require("./customer");
 const Components = require("./components");
 const Supplier = require("./supplier");
-
 const router = express.Router();
-
-//POSTS
-
-router.post("/customer/", (req, res) => {
-
-  const { name, phoneNumber, orderDate, furnitureOrder } = req.body;
-
-  const customer = new Customer({
-    name,
-    phoneNumber,
-    orderDate,
-    furnitureOrder,
-  });
-
-  try {
-    const dataToSave = customer.save();
-    res.status(200).json(dataToSave);
-  } catch (err) {
-    res.status(400).json({ message: err.message });
-  }
-});
-
-router.post("/supplier/", (req, res) => {
-  const { name, phoneNumber, materialsOrderNo, orderDate, deliveryDate } =
-    req.body;
-
-  const supplier = new Supplier({
-    name,
-    phoneNumber,
-    materialsOrderNo,
-    orderDate,
-    deliveryDate,
-  });
-
-  try {
-    const dataToSave = supplier.save();
-
-    res.status(200).json(dataToSave);
-  } catch (err) {
-    res.status(400).json({ message: err.message });
-  }
-});
 
 router.post("/components/", (req, res) => {
   const { id, component, stockLevel, triggerPoint } = req.body;
@@ -66,46 +23,9 @@ router.post("/components/", (req, res) => {
   }
 });
 
-//GETS
-router.get("/customer/", async (req, res) => {
-  try {
-    const data = await Customer.find();
-    res.json(data);
-  } catch (error) {
-    res.status(500).json({ message: error.message });
-  }
-});
-
-router.get("/supplier/", async (req, res) => {
-  try {
-    const data = await Supplier.find();
-    res.json(data);
-  } catch (error) {
-    res.status(500).json({ message: error.message });
-  }
-});
-
 router.get("/components/", async (req, res) => {
   try {
     const data = await Components.find();
-    res.json(data);
-  } catch (error) {
-    res.status(500).json({ message: error.message });
-  }
-});
-
-router.get("/customer/:id", async (req, res) => {
-  try {
-    const data = await Customer.findById(req.params.id);
-    res.json(data);
-  } catch (error) {
-    res.status(500).json({ message: error.message });
-  }
-});
-
-router.get("/supplier/:id", async (req, res) => {
-  try {
-    const data = await Supplier.findById(req.params.id);
     res.json(data);
   } catch (error) {
     res.status(500).json({ message: error.message });
@@ -120,8 +40,6 @@ router.get("/components/:id", async (req, res) => {
     res.status(500).json({ message: error.message });
   }
 });
-
-//PATCH
 
 router.patch('/components/:id', async (req, res) => {
   try {
@@ -139,8 +57,6 @@ router.patch('/components/:id', async (req, res) => {
       res.status(400).json({ message: error.message })
   }
 })
-
-//DELETE
 
 router.delete('/components/:id', async (req, res) => {
   try {
